a1ac5d30b2ffda5f828e8ebae3dc5398da3428ad
vita <dragic.hsieh@gmail.com>
2018-05-25 16:49:43 +0800

    Done add post new and create actions and views for author

--- app/controllers/author/posts_controller.rb
+++ app/controllers/author/posts_controller.rb
@@ -4,4 +4,25 @@ class Author::PostsController < ApplicationController
     @posts = Post.order("created_at DESC")
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post 建立成功"
+      redirect_to author_posts_path
+    else
+      flash[:alert] = "Post 建立不成功"
+      render "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :description)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+  # 驗證 title 及 content 欄位必填
+  validates :title, :body, :description, presence: true
+
 end
