0dbfabfaf8c2f021f7e1d1b8ad926c547d9b8776
vita <dragic.hsieh@gmail.com>
2018-05-26 09:17:27 +0800

    Done set post resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # 前台路由
+  # 前台首頁網址
   root "posts#index"
 
-  # 後台路由
+  # posts 前台路由
+  resources :posts, only: [:index, :show]
+
   namespace :author do
+    # posts 後台路由
     resources :posts
+    # 後台首頁網址
     root "posts#index"
   end
 
