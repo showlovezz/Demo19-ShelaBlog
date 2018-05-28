ff9e5077bdb3bd733bc665ecde3abda32ba0b4fd
vita <dragic.hsieh@gmail.com>
2018-05-29 00:11:39 +0800

    Done create、show action and pages for for author

--- app/controllers/author/portfolios_controller.rb
+++ app/controllers/author/portfolios_controller.rb
@@ -1,11 +1,37 @@
 class Author::PortfoliosController < ApplicationController
 
+  before_action :set_portfolio, only: [:show]
+
   def index
     @portfolios = Portfolio.all
   end
 
   def new
     @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      flash[:notice] = "Portfolio 建立成功"
+      redirect_to author_portfolios_path
+    else
+      flash[:alert] = "Portfolio 建立不成功"
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :body, :description, :image)
+  end
+
+  def set_portfolio
+    @portfolio = Portfolio.friendly.find(params[:id])
   end
 
 end
