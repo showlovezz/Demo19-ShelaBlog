07d94e2048aab873b4c39300ce21448a9b78f355
vita <dragic.hsieh@gmail.com>
2018-05-29 00:26:50 +0800

    Done Portfolio index、show action and pages for public users

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,11 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolios = Portfolio.order("created_at DESC")
+  end
+
+  def show
+    @portfolio = Portfolio.friendly.find(params[:id])
+  end
+
 end
